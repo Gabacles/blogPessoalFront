e5adce14cfcc3434c92d4c93978259b5c3cb7b1c
Gabacles <g.dalrovere@gmail.com>
2022-06-01 13:15:22 -0300

    Implementação de consumo de api Cadastrar e Logar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
-  
+
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
