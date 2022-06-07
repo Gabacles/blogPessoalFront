cbf7ede10e7e5c6cc4895b609ed0473074b84c2b
Gabacles <g.dalrovere@gmail.com>
2022-06-07 14:34:33 -0300

    Implementação do post e get Temas

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,3 +1,6 @@
+import { Tema } from './../model/Tema';
+import { Observable } from 'rxjs';
+import { TemaService } from './../service/tema.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
@@ -5,21 +8,36 @@ import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  temas: Tema[];
+  novoTema: Tema = new Tema();
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router, private temaService: TemaService) {}
 
   ngOnInit() {
-
-    if (environment.token == ''){
-      alert('Sua sessão expirou, faça o login novamente.')
-      this.router.navigate(['/entrar'])
+    if (environment.token == '') {
+      alert('Sua sessão expirou, faça o login novamente.');
+      this.router.navigate(['/entrar']);
     }
 
+    this.findAllTema();
   }
 
+  findAllTema() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.temas = resp;
+    });
+  }
+
+  cadastrar() {
+    console.log(this.novoTema)
+    this.temaService.postTema(this.novoTema).subscribe((resp: Tema) => {
+      this.novoTema = resp;
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTema();
+      this.novoTema = new Tema();
+    });
+  }
 }
