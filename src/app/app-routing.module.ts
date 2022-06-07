a98b064c5c12072cf3ce47f670c316d4154765fe
Gabacles <g.dalrovere@gmail.com>
2022-06-07 15:42:11 -0300

    Implementação de put e delete tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaComponent } from './tema/tema.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
@@ -6,17 +8,20 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
 
-  {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent},
+  { path: 'entrar', component: EntrarComponent },
+  { path: 'cadastrar', component: CadastrarComponent },
 
   { path: 'home', component: HomeComponent },
-  { path: 'tema', component: TemaComponent }
+  { path: 'tema', component: TemaComponent },
+
+  { path: 'tema-edit/:id', component: TemaEditComponent },
+  { path: 'tema-delete/:id', component: TemaDeleteComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -18,6 +18,10 @@ export class TemaService {
     return this.http.get<Tema[]>('http://localhost:8080/temas', this.token);
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>(
       'http://localhost:8080/temas/cadastrar',
@@ -25,4 +29,20 @@ export class TemaService {
       this.token
     );
   }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>(
+      'http://localhost:8080/temas/atualizar',
+      tema,
+      this.token
+    );
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(
+      `http://localhost:8080/temas/${id}`,
+      this.token
+    );
+  }
+
 }
