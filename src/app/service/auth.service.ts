c1fe1d21c80588187898f22595dfb9e3e27e465c
Gabacles <g.dalrovere@gmail.com>
2022-06-08 11:14:02 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserLogin } from './../model/UserLogin';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
@@ -11,27 +11,40 @@ import { environment } from 'src/environments/environment.prod';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
   entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>(
       'http://localhost:8080/usuarios/logar',
       userLogin
-    )
+    );
   }
 
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>(
       'http://localhost:8080/usuarios/cadastrar',
       user
-    )
+    );
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    console.log(environment.token)
+    console.log(this.token);
+    return this.http.get<User>(
+      `http://localhost:8080/usuarios/${id}`,
+      {headers: new HttpHeaders().set('Authorization', environment.token)}
+    );
   }
 
   logado() {
-    let ok = false
+    let ok = false;
 
     if (environment.token != '') {
-      ok = true
+      ok = true;
     }
 
-    return ok
+    return ok;
   }
 }
---
+++ src/app/service/postagem.service.ts
@@ -0,0 +1,29 @@
+import { Postagem } from './../model/Postagem';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  getAllPostagem(): Observable<Postagem[]>{
+    console.log(this.token)
+    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+  }
+
+  postPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.post<Postagem>('http://localhost:8080/postagens/cadastrar', postagem, this.token)
+  }
+
+}
